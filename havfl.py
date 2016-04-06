eba2b6621a71b8f021647d3c4db0e7d7829ab8d0
James Davenport <jradavenport@gmail.com>
2016-04-06 13:22:38 -0700

    add output file for comparison of lamost data

--- havfl.py
+++ havfl.py
@@ -20,7 +20,7 @@ match Amy's M dwarf tables to the RA,Dec
 Marcel will then use these to look for serendipitous X-ray detections
 '''
 
-Mfile = 'amy/all_m.txt'
+Mfile = 'amy/all_m.csv'
 kic_m, kic_mper = np.loadtxt(Mfile, delimiter=',', usecols=(0,1), skiprows=1, unpack=True)
 
 ra_m = np.zeros_like(kic_m)-99
@@ -52,16 +52,23 @@ hdata = pd.read_csv(hfile)
 
 ewha_m = np.zeros_like(kic_m)-99
 ewhaerr_m = np.zeros_like(kic_m)-99
+file_L = np.empty(len(kic_m), dtype='S30')
+
 for k in range(len(kic_m)):
     mtc = np.where(ra_m[k] == hdata['col1'].values)
     if len(mtc[0])>0:
         ewha_m[k] = hdata['ewHa '].values[mtc][0]
         ewhaerr_m[k] = hdata['ewerr '].values[mtc][0]
+        file_L[k] = hdata['eyename'].values[mtc][0]
 
 # only the M dwarfs in our sample with valid EWHA measurements
 ok = np.where((ewha_m > -99) & (ewhaerr_m < 100))
 ok1 = np.where((ewha_m > -99) & (ewhaerr_m < 100) & (kic_mper > 0))
 ok0 = np.where((ewha_m > -99) & (ewhaerr_m < 100) & (kic_mper <= 0))
+
+# dfout = pd.DataFrame(data={'kicnum':kic_m[ok], 'Lfile':file_L[ok]})
+# dfout.to_csv('lamost_matches.csv')
+
 
 # http://adsabs.harvard.edu/abs/2014ApJ...795..161D
 # file from here: https://figshare.com/articles/Chi_values_for_K_and_M_dwarfs_Table_8_in_Douglas_14_/1275226
@@ -124,6 +131,13 @@ print('# LAMOST stars without Prot meas: ' + str(len(ok0[0])))
 print('# Tessa stars with Prot meas: ' + str(len(okt1[0])))
 print('# Tessa stars without Prot meas: ' + str(len(okt0[0])))
 
+mm = 0
+for k in range(len(ok)):
+    xm = np.where((okt[0] == ok[0][k]))
+    if len(xm[0])>0:
+        mm = mm + 1
+print('# of stars from LAMOST with Tessa data: '+str(mm))
+
 
 plt.figure()
 # LAMOST stars
@@ -133,16 +147,22 @@ plt.errorbar(lha_lbol_m[ok], np.log10(fl_m[ok]), xerr=lha_lbolerr_m[ok],
 # plt.scatter(lha_lbol_m[ok1], np.log10(fl_m[ok1]), c='k', marker='*', s=40)
 
 # data for Tessa's stars
-plt.scatter(lha_lbol_mt[okt], np.log10(fl_m[okt]), c='r')
+plt.scatter(lha_lbol_mt[okt], np.log10(fl_m[okt]), c='r', alpha=0.7)
 # plt.scatter(lha_lbol_mt[okt1], np.log10(fl_m[okt1]), c='r', marker='*', s=40)
 
 # data for GJ 1243, GJ 1245A, GJ 1245B
 plt.scatter([10**-3.56, 10**-4.14, 10**-3.97],
-            [-3.78, -3.93, -4.00], marker='^', s=40)
+            [-3.78, -3.93, -4.00], marker='^', alpha=0.5)
 
 plt.xlim(-0.001, 0.003)
 plt.ylim(-9,-2)
 plt.xlabel(r'LH$\alpha$/L$_{bol}$')
 plt.ylabel('log L$_{flare}$/L$_{kp}$')
-plt.savefig('flare_vs_lhalbol.png',dpi=300)
+plt.savefig('flare_vs_lhalbol.png',dpi=150)
 plt.close()
+
+
+# plt.figure()
+# plt.scatter(kic_mper[ok], lha_lbol_m[ok], c='k')
+# plt.scatter(kic_mper[okt], lha_lbol_mt[okt], c='r')
+# plt.show()
